61272f189cc8dbecdbd5c8d71e5175351bc3b716
Wilbert Ribeiro <wilbert@khronus.com.br>
2011-07-07 12:01:19 -0300

    Corrigindo bugs

--- lib/generators/nifty/scaffold/templates/actions/destroy.rb
+++ lib/generators/nifty/scaffold/templates/actions/destroy.rb
@@ -2,7 +2,7 @@
     @<%= instance_name %> = <%= class_name %>.find(params[:id])
     @<%= instance_name %>.destroy
     
-    respond_with |format|
+    respond_with do |format|
       format.html { redirect_to <%= items_url %>, :notice => t(:destroyed, :model => t(:<%= instance_name %>, :scope => "activerecord.models").downcase) }
     end
   end
